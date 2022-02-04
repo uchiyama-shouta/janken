37771aad71832597bcd7da3a01bf8a2212ea7528
utiyama <shou0604pro@icloud.com>
2022-02-04 09:19:05 +0900

    AVIF化

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
+/** @type {import('next').NextConfig} */
+const config = {
+  swcMinify: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+};
+
 module.exports = withPWA({
   pwa: {
     dest: "public",
     runtimeCaching,
   },
+  ...config,
 });
